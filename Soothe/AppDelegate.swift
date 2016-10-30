e536d0741cf9750a14added6057b2a78ba7905a0
Benoit Sarrazin <ben@berzerker.io>
2016-10-30 12:20:13 -0400

    First project structure

---
+++ Soothe/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Soothe
+//
+//  Created by Benoit Sarrazin on 2016-10-30.
+//  Copyright © 2016 Berzerker IO. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Soothe/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Soothe
+//
+//  Created by Benoit Sarrazin on 2016-10-30.
+//  Copyright © 2016 Berzerker IO. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
