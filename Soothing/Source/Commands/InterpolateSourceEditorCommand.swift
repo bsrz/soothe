b5420779d98b3bb74ecc2ac1f34293b38b3ae311
Ben Sarrazin <b@srz.io>
2020-06-11 17:28:03 -0400

    Better interpolation

--- Soothing/Source/Commands/InterpolateSourceEditorCommand.swift
+++ Soothing/Source/Commands/InterpolateSourceEditorCommand.swift
@@ -4,26 +4,35 @@ import XcodeKit
 class InterpolateSourceEditorCommand: NSObject, XCSourceEditorCommand {
 
     func perform(with invocation: XCSourceEditorCommandInvocation, completionHandler: @escaping (Error?) -> Void ) {
-        let range = invocation.buffer.selections.firstObject as? XCSourceTextRange
         guard
-            let start = range?.start,
-            let end = range?.end,
-            start.line == end.line,
-            invocation.buffer.lines.count >= end.line,
-            var line = invocation.buffer.lines[start.line] as? String
-            else {
-                let userInfo = [NSLocalizedDescriptionKey: "This extension works only with a single selection, on a single line."]
-                let error = NSError(domain: #function, code: #line, userInfo: userInfo)
-                return completionHandler(error)
-            }
+            let selection = invocation.buffer.selections.firstObject as? XCSourceTextRange,
+            var line = invocation.buffer.lines[selection.start.line] as? String
+            else { return completionHandler(InterpolateError.noSelection) }
 
-        let endIndex = line.index(line.startIndex, offsetBy: end.column)
-        line.insert(")", at: endIndex)
+        guard selection.start.line == selection.end.line else {
+            return completionHandler(InterpolateError.multilineSelection)
+        }
 
-        let startIndex = line.index(line.startIndex, offsetBy: start.column)
-        line.insert(contentsOf: ["\\", "("], at: startIndex)
+        line.insert(")", at: line.index(line.startIndex, offsetBy: selection.end.column))
+        line.insert("(", at: line.index(line.startIndex, offsetBy: selection.start.column))
+        line.insert(#"\"#, at: line.index(line.startIndex, offsetBy: selection.start.column))
+        invocation.buffer.lines.replaceObject(at: selection.start.line, with: line)
 
-        invocation.buffer.lines.replaceObject(at: start.line, with: line)
+        selection.start.column += 2
+        selection.end.column += 2
+
         completionHandler(nil)
+    }
+}
+
+enum InterpolateError: Error {
+    case noSelection
+    case multilineSelection
+
+    var localizedDescription: String {
+        switch self {
+        case .noSelection: return "This command only works when you have a single text selection."
+        case .multilineSelection: return "This command only works when you have a single text selection."
+        }
     }
 }
